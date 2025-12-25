cd393440239eb10125cbe8235f628b67d1ee39cc
LianMary <liana.maria230604@gmail.com>
2025-12-25 11:52:27 -0300

    arquivos iniciais

---
+++ Sintax/SintaxBasica.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+package Sintax;
+
+public class SintaxBasica {
+    /* Escreva um código que receba o nome e o ano de nascimento de alguém
+    e imprima na tela a seguinte mensagem: "Olá 'Fulano' você tem 'X' anos"*/
+
+    public static void main(String[] args){
+        Scanner entra = new Scanner (System.in);
+
+        String nome;
+        int anoNascimento;
+
+        System.out.println("Digite seu nome:");
+        nome = entra.nextLine();
+
+        System.out.println("Digite seu ano de nascimento:");
+        anoNascimento = entra.nextInt();
+
+        int idade = 2025 - anoNascimento;
+
+        System.out.println("Olá " + nome + " você tem " + idade + " anos");
+    }
+}
